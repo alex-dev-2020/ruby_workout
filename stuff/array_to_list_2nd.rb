8bfabc6978b0213c8f7008980a8f41169a71bef2
alexfmjpn <lexfnjpm@mail.ru>
2020-02-24 11:10:23 +0000

    Deleted array_to_list.rb  and fixed some syntax

--- stuff/array_to_list_2nd.rb
+++ stuff/array_to_list_2nd.rb
@@ -1,14 +1,7 @@
-# друга, более изящная реализация array to list после code review
-# через each. wiith_index(offset)
-#  про offset объяснение есть здесь^
+# другая, более изящная реализация array to list после code review
+# через each.with_index(offset)
+# про offset объяснение есть здесь:
 # https://stackoverflow.com/questions/5646390/ruby-each-with-index-offset/19567086
-# =begin
-# разворачиваю массив построчно (для ДЗ к уроку 6 - при назначении маршрута поезду)
-# сначала нужно вывести список маршрутов построчно
-# затем сросить пользователя - какой именно маршрут назначаем поезду (номер)
-# вместо source подставим my_ralroad.routes
-#   print index + 1    1-й лемент массива имеет индекс 0 
-# =end
 
 # создаем массив букв, перемешиваем
 source = ('a'..'z').sort_by{rand}
